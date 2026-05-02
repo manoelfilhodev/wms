6daeb3b35592b1048b86022619aa9ba428a1542e
Manoel Filho <manoel.filho@systex.com.br>
2026-05-01 23:22:16 -0300

    Base finalizada para subir em produção

--- public/index.php
+++ public/index.php
@@ -4,14 +4,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$appBasePath = realpath(__DIR__.'/../laravel-tpc-bombril') ?: realpath(__DIR__.'/..');
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $appBasePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $appBasePath.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once $appBasePath.'/bootstrap/app.php')
     ->handleRequest(Request::capture());
